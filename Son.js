9aebf4bfaeb4f7a6f07afa362290a5908f340501
BlackBulletBrony <51234766+BlackBulletBrony@users.noreply.github.com>
2019-12-04 14:58:41 +0500

    Update Son.js

--- Son.js
+++ Son.js
@@ -1,8 +1,7 @@
 console.log("Loading...");
 (function(){
 	let e = document.createElement(`script`);
-	e.src = `https://BlackBulletBrony.github.io/FuckYou.js`;
-	//e.src = `https://BlackBulletBrony.github.io/MLPP_Minimap.user.js`;
+	e.src = `https://endlessnightnlr.github.io/MLPP/MLPP_Minimap.user.js`;
 	//e.src = `https://Autumn-Blaze.github.io/MLPP_Minimap.user.js`;
 	document.body.appendChild(e);
 })();
