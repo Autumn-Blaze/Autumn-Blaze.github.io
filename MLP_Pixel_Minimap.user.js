fefcff8f40b121b84b9efd6b6802cf2682a65fab
BlackBulletBrony <51234766+BlackBulletBrony@users.noreply.github.com>
2020-03-26 15:07:52 +0500

    Update MLP_Pixel_Minimap.user.js

--- MLP_Pixel_Minimap.user.js
+++ MLP_Pixel_Minimap.user.js
@@ -1,12 +1,18 @@
 // ==UserScript==
 // @name         MLP Pixel Minimap
 // @namespace    http://tampermonkey.net/
-// @version      1.2.9
-// @description  My Little Pony Pixel (MLP Pixel) Minimap
+// @version      1.3.0
+// @description  MLP Pixel Minimap
 // @author       Endless Night (and ConsoleBey)
 // @include      *://pixelzone.io/*
-// @include      *://pixel.vkforms.ru/*
+// @include      *://pixelplanet.fun/*
+// @include      *://fuckyouarkeros.fun/*
 // @include      *://pixel2019.vkforms.ru/*
+// @include      *://pixel2020.vkforms.ru/*
+// @include      *://ourworldofpixels.com/*
+// @include      *://pixelplace.io/*
+// @include      *://pxls.space/*
+// @include      *://goodsanta.club/*
 // @homepage     https://Autumn-Blaze.github.io
 // @updateURL    https://Autumn-Blaze.github.io/MLP_Pixel_Minimap.user.js
 // @downloadURL  https://Autumn-Blaze.github.io/MLP_Pixel_Minimap.user.js
@@ -15,14 +21,17 @@
 // To the glory of Luna and the New Lunar Republic!
 // Improved by the Endless Night.
 //
-console.log("Run mlpp_minimap.exe");
-(()=>{
-	let a = {
-			'pixel2019.vkforms.ru' : 'https://endlessnightnlr.github.io/master/MLPP/pb/who.js',
-			'pixelzone.io' : 'https://Autumn-Blaze.github.io/Son.js'
-//			'pixelzone.io' : 'https://Autumn-Blaze.github.io/MLPP_Minimap.user.js'
-		},
-		e = document.createElement(`script`);
-	if(e.src = a[window.location.host])document.body.appendChild(e);
-	else console.warn('SOURCE CODE FOR INJECTION NOT FOUND');
-})();
+{
+	let e = document.createElement('script');
+	(e.src = {
+		'pixelzone.io' : 'https://Autumn-Blaze.github.io/Son.js',
+		'pixelplanet.fun' : 'https://endlessnightnlr.github.io/MLPP/PixelPlanet/code.js',
+		'fuckyouarkeros.fun' : 'https://endlessnightnlr.github.io/MLPP/PixelPlanet/code.js',
+		'pixel2019.vkforms.ru' : 'https://endlessnightnlr.github.io/master/MLPP/pb2019/who.js',
+		'pixel2020.vkforms.ru' : 'https://endlessnightnlr.github.io/master/MLPP/pb2020/who.js',
+		'ourworldofpixels.com' : 'https://endlessnightnlr.github.io/MLPP/OWOP/code.js',
+		'pixelplace.io' : 'https://endlessnightnlr.github.io/MLPP/PixelPlace/code.js',
+		'pxls.space' : 'https://endlessnightnlr.github.io/MLPP/PxlsSpace/code.js',
+		'goodsanta.club' : 'https://endlessnightnlr.github.io/MLPP/MiniPixel/code.js'
+	}[window.location.host]) ? document.body.appendChild(e) : console.warn('SOURCE CODE FOR INJECTION NOT FOUND');
+};
