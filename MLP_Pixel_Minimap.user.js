f33b754b619f63db44f5c2256a5130eafbcaf240
BlackBulletBrony <51234766+BlackBulletBrony@users.noreply.github.com>
2020-10-10 13:55:07 +0500

    Update MLP_Pixel_Minimap.user.js

--- MLP_Pixel_Minimap.user.js
+++ MLP_Pixel_Minimap.user.js
@@ -1,14 +1,16 @@
 // ==UserScript==
 // @name         MLP Pixel Minimap
 // @namespace    http://tampermonkey.net/
-// @version      1.3.0
+// @version      1.3.1
 // @description  MLP Pixel Minimap
 // @author       Endless Night (and ConsoleBey)
 // @include      *://pixelzone.io/*
 // @include      *://pixelplanet.fun/*
 // @include      *://fuckyouarkeros.fun/*
+// @include      *://vk.com/*
 // @include      *://pixel2019.vkforms.ru/*
 // @include      *://pixel2020.vkforms.ru/*
+// @include      *://pixel.w84.vkforms.ru/*
 // @include      *://ourworldofpixels.com/*
 // @include      *://pixelplace.io/*
 // @include      *://pxls.space/*
@@ -27,8 +29,9 @@
 		'pixelzone.io' : 'https://Autumn-Blaze.github.io/Son.js',
 		'pixelplanet.fun' : 'https://endlessnightnlr.github.io/MLPP/PixelPlanet/code.js',
 		'fuckyouarkeros.fun' : 'https://endlessnightnlr.github.io/MLPP/PixelPlanet/code.js',
-		'pixel2019.vkforms.ru' : 'https://endlessnightnlr.github.io/master/MLPP/pb2019/who.js',
-		'pixel2020.vkforms.ru' : 'https://endlessnightnlr.github.io/master/MLPP/pb2020/who.js',
+		'pixel2019.vkforms.ru' : 'https://endlessnightnlr.github.io/MLPP/pb/code.js',
+		'pixel2020.vkforms.ru' : 'https://endlessnightnlr.github.io/MLPP/pb/code.js',
+		'pixel.w84.vkforms.ru' : 'https://endlessnightnlr.github.io/MLPP/pb/code.js',
 		'ourworldofpixels.com' : 'https://endlessnightnlr.github.io/MLPP/OWOP/code.js',
 		'pixelplace.io' : 'https://endlessnightnlr.github.io/MLPP/PixelPlace/code.js',
 		'pxls.space' : 'https://endlessnightnlr.github.io/MLPP/PxlsSpace/code.js',
