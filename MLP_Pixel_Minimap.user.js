1aa60a93a5de49a8ddf38894c8dc7227b0a0a0c1
EndlessNightNLR <48591997+EndlessNightNLR@users.noreply.github.com>
2021-02-23 19:39:27 +0500

    Update MLP_Pixel_Minimap.user.js

--- MLP_Pixel_Minimap.user.js
+++ MLP_Pixel_Minimap.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MLP Pixel Minimap
 // @namespace    http://tampermonkey.net/
-// @version      1.3.4
+// @version      1.3.5
 // @description  MLP Pixel Minimap
 // @author       Endless Night (and ConsoleBey)
 // @include      *://pixelzone.io/*
@@ -16,6 +16,7 @@
 // @include      *://pxls.space/*
 // @include      *://goodsanta.club/*
 // @include      https://prod-app*
+// @include      https://pixelwar-mts.ru/*
 // @homepage     https://Autumn-Blaze.github.io
 // @updateURL    https://Autumn-Blaze.github.io/MLP_Pixel_Minimap.user.js
 // @downloadURL  https://Autumn-Blaze.github.io/MLP_Pixel_Minimap.user.js
@@ -32,8 +33,7 @@
 		return;
 	};
 
-	let e = document.createElement('script');
-	(e.src = {
+	const pathToScript = {
 		'pixelzone.io' : 'https://Autumn-Blaze.github.io/Son.js',
 		'pixelplanet.fun' : 'https://endlessnightnlr.github.io/MLPP/PixelPlanet/code.js',
 		'fuckyouarkeros.fun' : 'https://endlessnightnlr.github.io/MLPP/PixelPlanet/code.js',
@@ -43,6 +43,18 @@
 		'ourworldofpixels.com' : 'https://endlessnightnlr.github.io/MLPP/OWOP/code.js',
 		'pixelplace.io' : 'https://endlessnightnlr.github.io/MLPP/PixelPlace/code.js',
 		'pxls.space' : 'https://endlessnightnlr.github.io/MLPP/PxlsSpace/code.js',
-		'goodsanta.club' : 'https://endlessnightnlr.github.io/MLPP/MiniPixel/code.js'
-	}[window.location.host]) ? document.body.appendChild(e) : console.warn('SOURCE CODE FOR INJECTION NOT FOUND');
+		'goodsanta.club' : 'https://endlessnightnlr.github.io/MLPP/MiniPixel/code.js',
+		'pixelwar-mts.ru': 'https://raw.githubusercontent.com/EndlessNightNLR/endlessnightnlr.github.io/master/MLPP/MTS%202021/code.js'
+	}[window.location.host];
+
+	if(pathToScript !== void 0){
+		fetch(pathToScript)
+		.then(res => res.text())
+		.then(code => {
+			const e = document.createElement('script');
+			e.innerHTML = code;
+			document.body.appendChild(e);
+
+		});
+	}
 };
