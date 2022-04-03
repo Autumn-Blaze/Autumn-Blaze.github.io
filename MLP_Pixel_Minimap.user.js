b00f613d3f68fcdb3e852e7395c21f56e2da5f09
EndlessNightNLR <48591997+EndlessNightNLR@users.noreply.github.com>
2022-04-03 12:44:09 +0500

    Update MLP_Pixel_Minimap.user.js

--- MLP_Pixel_Minimap.user.js
+++ MLP_Pixel_Minimap.user.js
@@ -15,54 +15,66 @@
 // @include      *://pixelplace.io/*
 // @include      *://pxls.space/*
 // @include      *://goodsanta.club/*
-// @include      *://www.reddit.com/r/place/*
+// @include      *://hot-potato.reddit.com/embed*
 // @include      https://prod-app*
 // @include      https://pixelwar-mts.ru/*
 // @homepage     https://Autumn-Blaze.github.io
 // @updateURL    https://Autumn-Blaze.github.io/MLP_Pixel_Minimap.user.js
 // @downloadURL  https://Autumn-Blaze.github.io/MLP_Pixel_Minimap.user.js
+// @grant        GM.xmlHttpRequest
+// @connect      localhost
+// @connect		 endlessnightnlr.github.io
+// @connect		 raw.githubusercontent.com
 // ==/UserScript==
 //
 // To the glory of Luna and the New Lunar Republic!
 // Improved by the Endless Night.
 //
-{
-	if(location.href.startsWith('https://prod-app')){
-		let e = document.createElement('script');
-		e.src = 'https://endlessnightnlr.github.io/MLPP/pb/code.js';
-		document.body.appendChild(e);
-		return;
-	};
-	
-	if(location.href.startsWith('https://www.reddit.com/r/place')){
-		let e = document.createElement('script');
-		e.src = 'https://endlessnightnlr.github.io/MLPP/rplace/code.js';
-		document.body.appendChild(e);
-		return;
-	};
 
-	const pathToScript = {
-		'pixelzone.io' : 'https://Autumn-Blaze.github.io/Son.js',
-		'pixelplanet.fun' : 'https://endlessnightnlr.github.io/MLPP/PixelPlanet/code.js',
-		'fuckyouarkeros.fun' : 'https://endlessnightnlr.github.io/MLPP/PixelPlanet/code.js',
-		'pixel2019.vkforms.ru' : 'https://endlessnightnlr.github.io/MLPP/pb/code.js',
-		'pixel2020.vkforms.ru' : 'https://endlessnightnlr.github.io/MLPP/pb/code.js',
-		'pixel.w84.vkforms.ru' : 'https://endlessnightnlr.github.io/MLPP/pb/code.js',
-		'ourworldofpixels.com' : 'https://endlessnightnlr.github.io/MLPP/OWOP/code.js',
-		'pixelplace.io' : 'https://endlessnightnlr.github.io/MLPP/PixelPlace/code.js',
-		'pxls.space' : 'https://endlessnightnlr.github.io/MLPP/PxlsSpace/code.js',
-		'goodsanta.club' : 'https://endlessnightnlr.github.io/MLPP/MiniPixel/code.js',
-		'pixelwar-mts.ru': 'https://raw.githubusercontent.com/EndlessNightNLR/endlessnightnlr.github.io/master/MLPP/MTS%202021/code.js'
-	}[window.location.host];
+if(location.href.startsWith('https://prod-app')){
+	let e = document.createElement('script');
+	e.src = 'https://endlessnightnlr.github.io/MLPP/pb/code.js';
+	document.body.appendChild(e);
+	return;
+};
 
-	if(pathToScript !== void 0){
-		fetch(pathToScript)
+if(location.href.startsWith('https://hot-potato.reddit.com/embed')){
+	const _TamperRoot = this;
+	(async function() {
+		GM.xmlHttpRequest({
+			method: "GET",
+			//url: 'https://endlessnightnlr.github.io/MLPP/rplace/code.js',
+			url: 'https://endlessnightnlr.github.io/MLPP/rplace/code2.js',
+			//url: 'http://localhost',
+			onload: function(res) {
+				new Function('const GM = arguments[0].GM;\n\n' + res.responseText)(_TamperRoot);
+			}
+		});
+	})();
+	return;
+};
+
+const pathToScript = {
+	'pixelzone.io' : 'https://Autumn-Blaze.github.io/Son.js',
+	'pixelplanet.fun' : 'https://endlessnightnlr.github.io/MLPP/PixelPlanet/code.js',
+	'fuckyouarkeros.fun' : 'https://endlessnightnlr.github.io/MLPP/PixelPlanet/code.js',
+	'pixel2019.vkforms.ru' : 'https://endlessnightnlr.github.io/MLPP/pb/code.js',
+	'pixel2020.vkforms.ru' : 'https://endlessnightnlr.github.io/MLPP/pb/code.js',
+	'pixel.w84.vkforms.ru' : 'https://endlessnightnlr.github.io/MLPP/pb/code.js',
+	'ourworldofpixels.com' : 'https://endlessnightnlr.github.io/MLPP/OWOP/code.js',
+	'pixelplace.io' : 'https://endlessnightnlr.github.io/MLPP/PixelPlace/code.js',
+	'pxls.space' : 'https://endlessnightnlr.github.io/MLPP/PxlsSpace/code.js',
+	'goodsanta.club' : 'https://endlessnightnlr.github.io/MLPP/MiniPixel/code.js',
+	'pixelwar-mts.ru': 'https://raw.githubusercontent.com/EndlessNightNLR/endlessnightnlr.github.io/master/MLPP/MTS%202021/code.js'
+}[window.location.host];
+
+if(pathToScript !== void 0){
+	fetch(pathToScript)
 		.then(res => res.text())
 		.then(code => {
-			const e = document.createElement('script');
-			e.innerHTML = code;
-			document.body.appendChild(e);
+		const e = document.createElement('script');
+		e.innerHTML = code;
+		document.body.appendChild(e);
 
-		});
-	}
-};
+	});
+}
