ef481274ae6e9289b3b22d4bcc993dc2f460e773
BlackBulletBrony <51234766+BlackBulletBrony@users.noreply.github.com>
2022-04-04 02:40:03 +0500

    Update MLP_Pixel_Minimap.user.js

--- MLP_Pixel_Minimap.user.js
+++ MLP_Pixel_Minimap.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MLP Pixel Minimap
 // @namespace    http://tampermonkey.net/
-// @version      1.3.6
+// @version      1.3.7
 // @description  MLP Pixel Minimap
 // @author       Endless Night (and ConsoleBey)
 // @include      *://pixelzone.io/*
