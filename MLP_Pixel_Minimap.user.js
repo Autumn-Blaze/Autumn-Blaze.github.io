94cf25b93a8227ecc5d43211fffe2c7c6b9d6a18
EndlessNightNLR <48591997+EndlessNightNLR@users.noreply.github.com>
2022-11-12 21:33:29 +0500

    Update MLP_Pixel_Minimap.user.js

--- MLP_Pixel_Minimap.user.js
+++ MLP_Pixel_Minimap.user.js
@@ -4,6 +4,17 @@
 // @version      1.3.7
 // @description  MLP Pixel Minimap
 // @author       Endless Night (and ConsoleBey)
+// @grant 		 GM_xmlhttpRequest
+// @grant 		 unsafeWindow
+// @require		 https://raw.githubusercontent.com/mitchellmebane/GM_fetch/master/GM_fetch.min.js
+// @connect		 githubusercontent.com
+// @connect		 github.io
+// @connect		 github.com
+// @connect      localhost
+// @connect		 glitch.me
+// @connect		 pixelzone.io
+// @connect		 pixelplanet.fun
+// @connect		 fuckyouarkeros.fun
 // @match      *://pixelzone.io/*
 // @match      *://pixelplanet.fun/*
 // @match      *://fuckyouarkeros.fun/*
@@ -21,60 +32,43 @@
 // @homepage     https://Autumn-Blaze.github.io
 // @updateURL    https://Autumn-Blaze.github.io/MLP_Pixel_Minimap.user.js
 // @downloadURL  https://Autumn-Blaze.github.io/MLP_Pixel_Minimap.user.js
-// @grant        GM.xmlHttpRequest
-// @connect      localhost
-// @connect		 endlessnightnlr.github.io
-// @connect		 raw.githubusercontent.com
+
 // ==/UserScript==
 //
 // To the glory of Luna and the New Lunar Republic!
 // Improved by the Endless Night.
-//
 
-if(location.href.startsWith('https://prod-app')){
-	let e = document.createElement('script');
-	e.src = 'https://endlessnightnlr.github.io/MLPP/pb/code.js';
-	document.body.appendChild(e);
-	return;
-};
-
-if(location.href.startsWith('https://hot-potato.reddit.com/embed')){
-	const _TamperRoot = this;
-	(async function() {
-		GM.xmlHttpRequest({
-			method: "GET",
-			//url: 'https://endlessnightnlr.github.io/MLPP/rplace/code.js',
-			url: 'https://endlessnightnlr.github.io/MLPP/rplace/code2.js',
-			//url: 'http://localhost',
-			onload: function(res) {
-				new Function('const GM = arguments[0].GM;\n\n' + res.responseText)(_TamperRoot);
+[
+	// ['https://prod-app*', 'https://endlessnightnlr.github.io/MLPP/pb/code.js'],
+	// ['https://hot-potato.reddit.com/embed*', 'https://endlessnightnlr.github.io/MLPP/rplace/code2.js'],
+	['.*:\/\/pixelzone\.io.*', 'https://Autumn-Blaze.github.io/Son.js'],
+	['.*:\/\/pixelplanet\.fun.*', 'https://endlessnightnlr.github.io/MLPP/PixelPlanet/code.js'],
+	['.*:\/\/fuckyouarkeros\.fun.*', 'https://endlessnightnlr.github.io/MLPP/PixelPlanet/code.js'],
+	// 'pixel2019.vkforms.ru' : 'https://endlessnightnlr.github.io/MLPP/pb/code.js',
+	// 'pixel2020.vkforms.ru' : 'https://endlessnightnlr.github.io/MLPP/pb/code.js',
+	// 'pixel.w84.vkforms.ru' : 'https://endlessnightnlr.github.io/MLPP/pb/code.js',
+	['.*:\/\/ourworldofpixels\.com.*', 'https://endlessnightnlr.github.io/MLPP/OWOP/code.js'],
+	['.*:\/\/pixelplace\.io.*', 'https://endlessnightnlr.github.io/MLPP/PixelPlace/code.js'],
+	['.*:\/\/pxls\.space.*', 'https://endlessnightnlr.github.io/MLPP/PxlsSpace/code.js'],
+	// 'goodsanta.club' : 'https://endlessnightnlr.github.io/MLPP/MiniPixel/code.js',
+	// 'pixelwar-mts.ru': 'https://raw.githubusercontent.com/EndlessNightNLR/endlessnightnlr.github.io/master/MLPP/MTS%202021/code.js'
+].forEach(([reg, src]) => {
+	if (new RegExp(reg).test(location.href)) {
+		fetch(src)
+		.then(res => {
+			if (res.readyState !== res.DONE) {
+				return;
 			}
-		});
-	})();
-	return;
-};
 
-const pathToScript = {
-	'pixelzone.io' : 'https://Autumn-Blaze.github.io/Son.js',
-	'pixelplanet.fun' : 'https://endlessnightnlr.github.io/MLPP/PixelPlanet/code.js',
-	'fuckyouarkeros.fun' : 'https://endlessnightnlr.github.io/MLPP/PixelPlanet/code.js',
-	'pixel2019.vkforms.ru' : 'https://endlessnightnlr.github.io/MLPP/pb/code.js',
-	'pixel2020.vkforms.ru' : 'https://endlessnightnlr.github.io/MLPP/pb/code.js',
-	'pixel.w84.vkforms.ru' : 'https://endlessnightnlr.github.io/MLPP/pb/code.js',
-	'ourworldofpixels.com' : 'https://endlessnightnlr.github.io/MLPP/OWOP/code.js',
-	'pixelplace.io' : 'https://endlessnightnlr.github.io/MLPP/PixelPlace/code.js',
-	'pxls.space' : 'https://endlessnightnlr.github.io/MLPP/PxlsSpace/code.js',
-	'goodsanta.club' : 'https://endlessnightnlr.github.io/MLPP/MiniPixel/code.js',
-	'pixelwar-mts.ru': 'https://raw.githubusercontent.com/EndlessNightNLR/endlessnightnlr.github.io/master/MLPP/MTS%202021/code.js'
-}[window.location.host];
+			if (res.status !== 200) {
+				alert(`cant load script\ncode: ${res.status}\nreason: ${res.statusText}`);
+				return;
+			}
 
-if(pathToScript !== void 0){
-	fetch(pathToScript)
-		.then(res => res.text())
+			return res.text()
+		})
 		.then(code => {
-		const e = document.createElement('script');
-		e.innerHTML = code;
-		document.body.appendChild(e);
-
-	});
-}
+			new Function("const [self, GM, unsafeWindow] = arguments;\n" + code)(self, GM, unsafeWindow);
+		});
+	}
+});
