aed4599eb0c563b0429b6774fa190568b0f034e4
EndlessNightNLR <48591997+EndlessNightNLR@users.noreply.github.com>
2023-04-03 18:52:39 +0500

    Update MLP_Pixel_Minimap.user.js

--- MLP_Pixel_Minimap.user.js
+++ MLP_Pixel_Minimap.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MLP Pixel Minimap
 // @namespace    http://tampermonkey.net/
-// @version      1.3.10
+// @version      1.3.11
 // @description  MLP Pixel Minimap
 // @author       Endless Night (and ConsoleBey)
 // @grant 		 GM_xmlhttpRequest
